36babf979a1542ea40f3e7944b13ce1d4840d21a
lcbiplove <lcbiplove1@gmail.com>
2020-12-12 08:54:11 +0545

    Initial commit

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,27 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const String kapiKey = '7fd00f086cb692e4a5cba1968776ca36';
+const String kopenMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getLocationFromCityName(String cityName) async {
+    NetworkHelper networkHelper =
+        NetworkHelper('$kopenMapURL?q=$cityName&appid=$kapiKey&units=metric');
+    var weatherData = await networkHelper.getDecodedData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$kopenMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$kapiKey&units=metric');
+    var weatherData = await networkHelper.getDecodedData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
